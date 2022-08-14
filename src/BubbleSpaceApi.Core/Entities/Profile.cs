e9b254dd89fec8f5566f9404b625f2d2b72ea8d5
arakakivl <arakakivl@proton.me>
2022-08-14 20:02:16 -0300

    Making default value for Bio property from Profile entity.

--- src/BubbleSpaceApi.Core/Entities/Profile.cs
+++ src/BubbleSpaceApi.Core/Entities/Profile.cs
@@ -7,7 +7,7 @@ public class Profile : IBaseEntity<Guid>
     public Guid Id { get; set; } = Guid.NewGuid();
 
     public string Username { get; set; } = null!;
-    public string Bio { get; set; } = null!;
+    public string Bio { get; set; } = "";
 
     public IQueryable<Question> Questions { get; set; } = new List<Question>().AsQueryable();
     public IQueryable<Answer> Answers { get; set; } = new List<Answer>().AsQueryable();
