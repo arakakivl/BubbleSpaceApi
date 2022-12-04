edddc61373fd44ccfebaf7098e993ff90db01132
arakakivl <arakakivl@proton.me>
2022-12-03 21:05:38 -0300

    Creating and fixing some tests

--- tests/BubbleSpaceApi.ApiTests/Controllers/AccountControllerTests.cs
+++ tests/BubbleSpaceApi.ApiTests/Controllers/AccountControllerTests.cs
@@ -36,10 +36,11 @@ public class AccountControllerTests
         };
 
         _sut.ControllerContext.HttpContext.Request.Headers.Authorization = "Bearer someSortOfJWTToken";
+        _sut.ControllerContext.HttpContext.Request.Cookies.Append(new KeyValuePair<string, string>("bsrfh", "eyx984ut9433hg"));
     }
 
     [Fact]
-    public async Task RegisterAsync_ShouldReturnOk_WhenExecuted()
+    public async Task RegisterAsync_ShouldReturnOk_WhenNotAuthenticated()
     {
         // Arrange
         var model = new RegisterUserInputModel()
@@ -49,11 +50,26 @@ public class AccountControllerTests
             Password = "someuser123"
         };
 
+        _authStub.Setup(x => x.IsAuthenticated(It.IsAny<string>())).Returns(false);
+
         // Act
         var result = await _sut.SignUpAsync(model);
 
         // Assert
         Assert.IsType<OkResult>(result);
+    }
+
+    [Fact]
+    public async Task RegisterAsync_ShouldReturnBadRequest_WhenAlreadyAuthenticated()
+    {
+        // Arrange
+        _authStub.Setup(x => x.IsAuthenticated(It.IsAny<string>())).Returns(true);
+
+        // Act
+        var result = await _sut.SignUpAsync(new RegisterUserInputModel());
+
+        // Assert
+        Assert.IsType<BadRequestObjectResult>(result);
     }
 
     [Fact]
@@ -74,11 +90,13 @@ public class AccountControllerTests
         _authStub.Setup(x => x.GenerateToken(claims, false)).Returns(Guid.NewGuid().ToString());
         _authStub.Setup(x => x.GenerateToken(claims, true)).Returns(Guid.NewGuid().ToString());
 
+        _authStub.Setup(x => x.IsAuthenticated(It.IsAny<string>())).Returns(false);
+
         // Act
         var result = await _sut.SignInAsync(model);
 
         // Assert
-        Assert.IsType<OkResult>(result);
+        Assert.IsType<OkObjectResult>(result);
     }
 
     [Fact]
@@ -92,9 +110,53 @@ public class AccountControllerTests
         };
 
         _senderStub.Setup(x => x.Send(It.IsAny<LoginUserCommand>(), default)).ReturnsAsync(Guid.Empty);
+        _authStub.Setup(x => x.IsAuthenticated(It.IsAny<string>())).Returns(false);
 
         // Act
         var result = await _sut.SignInAsync(model);
+
+        // Assert
+        Assert.IsType<BadRequestObjectResult>(result);
+    }
+
+    [Fact]
+    public async Task LoginAsync_ShouldReturnBadRequest_WhenAlreadyAuthenticated()
+    {
+        // Arrange
+        _authStub.Setup(x => x.IsAuthenticated(It.IsAny<string>())).Returns(true);
+
+        // Act
+        var result = await _sut.SignInAsync(new LoginUserInputModel());
+
+        // Assert
+        Assert.IsType<BadRequestObjectResult>(result);
+    }
+
+    [Fact]
+    public async Task RefreshAsync_ShouldReturnOk_WhenNotAuthenticated()
+    {
+        // Arrange
+        _authStub.Setup(x => x.GetProfileIdFromToken(It.IsAny<string>())).Returns(Guid.NewGuid());
+        _authStub.Setup(x => x.IsAuthenticated(It.IsAny<string>())).Returns(false);
+        
+        _authStub.Setup(x => x.GenerateToken(It.IsAny<Dictionary<string, string>>(), false)).Returns(Guid.NewGuid().ToString());
+        _authStub.Setup(x => x.GenerateToken(It.IsAny<Dictionary<string, string>>(), true)).Returns(Guid.NewGuid().ToString());
+
+        // Act
+        var result = await _sut.RefreshAsync();
+
+        // Assert
+        Assert.IsType<OkObjectResult>(result);
+    }
+
+    [Fact]
+    public async Task RefreshAsync_ShouldReturnBadRequest_WhenAlreadyAuthenticated()
+    {
+        // Arrange
+        _authStub.Setup(x => x.IsAuthenticated(It.IsAny<string>())).Returns(true);
+
+        // Act
+        var result = await _sut.RefreshAsync();
 
         // Assert
         Assert.IsType<BadRequestObjectResult>(result);
